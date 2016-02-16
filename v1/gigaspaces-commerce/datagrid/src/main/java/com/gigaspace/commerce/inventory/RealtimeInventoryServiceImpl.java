59a2de2f26f3ccccc1341d6073e79f7442bbf812
michaelraney <raneymike83@gmail.com>
2016-02-15 20:45:49 -0500

    NYCO Demo
    Integrated with MULE ESB. Punchase functions were moved to separate
    provider

--- v1/gigaspaces-commerce/datagrid/src/main/java/com/gigaspace/commerce/inventory/RealtimeInventoryServiceImpl.java
+++ v1/gigaspaces-commerce/datagrid/src/main/java/com/gigaspace/commerce/inventory/RealtimeInventoryServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.gigaspaces.commerce.oracle.Inventory;
 import com.gigaspaces.commerce.oracle.RealtimeInventoryService;
 import com.j_spaces.core.client.SQLQuery;
-
+import com.mycompany.model.InventoryDemand;
 /**
  * @author Aliaksei Shary (EPAM Systems)
  */
@@ -23,59 +23,107 @@ public class RealtimeInventoryServiceImpl implements RealtimeInventoryService {
 	public int purchase(String catalogRefId, long howMany, String locationId) {
 		// TODO: Wrap each method in transaction from InventoryEndpoint
 		// TODO: Pessimistic Inventory Locking for updates - EXCLUSIVE_READ_LOCK
-		Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
-		return updateStockLevel(inventory, -howMany);
+		System.out.println("RealtimeInventoryServiceImpl purchase: " + catalogRefId + ", Location: " + locationId + ", howMany: " + howMany);
+    	
+    	Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
+		InventoryDemand inventoryDemand = readInventoryDemandByCatalogRefIdLocationId(catalogRefId, locationId);
+		
+		int result = validateInventoryRequest(inventory, inventoryDemand, -howMany);
+		
+		System.out.println("RealtimeInventoryServiceImpl purchase result :" + result);
+		
+		return result;
+		 
 	}
 
 	@Override
 	public long queryStockLevel(String catalogRefId, String locationId) {
-		long result = 0L;
+		System.out.println("RealtimeInventoryServiceImpl queryStockLevel: " + catalogRefId + ", Location: " + locationId );
+		
 		Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
-		if (inventory != null) {
-			result = inventory.getStockLevel();
-		}
+		InventoryDemand inventoryDemand = readInventoryDemandByCatalogRefIdLocationId(catalogRefId, locationId);
+		
+		long inventoryTotal = (inventory != null)?inventory.getStockLevel():0L;
+		long inventoryDemandTotal = (inventoryDemand != null)?inventoryDemand.getStockLevel():0L;
+		
+		long result = inventoryTotal - inventoryDemandTotal;
+		
+		System.out.println("RealtimeInventoryServiceImpl queryStockLevel result :" + result);
+		
 		return result;
 	}
 	
 	@Override
 	public long queryStockThreshold(String catalogRefId, String locationId) {
+		System.out.println("RealtimeInventoryServiceImpl queryStockThreshold: " + catalogRefId + ", Location: " + locationId );
+		
 		long result = 0L;
+		
 		Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
 		if (inventory != null) {
 			result = inventory.getStockThreshold();
 		}
+		
+		System.out.println("RealtimeInventoryServiceImpl queryStockThreshold result :" + result);
+		
 		return result;
 	}
 
 	@Override
 	public int queryAvailabilityStatus(String catalogRefId, String locationId) {
+		System.out.println("RealtimeInventoryServiceImpl queryAvailabilityStatus: " + catalogRefId + ", Location: " + locationId );
+		
 		int result = AVAILABILITY_STATUS_OUT_OF_STOCK;
+		
 		Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
 		if (inventory != null) {
 			result = inventory.getAvailabilityStatus();
 		}
+		
+		System.out.println("RealtimeInventoryServiceImpl queryAvailabilityStatus result :" + result);
+		
 		return result;
 	}
 	
 	@Override
 	public int decreaseStockLevel(String catalogRefId, long howMany, String locationId) {
-		Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
-		return updateStockLevel(inventory, -howMany);
+		System.out.println("RealtimeInventoryServiceImpl decreaseStockLevel: " + catalogRefId + ", Location: " + locationId + ", howMany: " + howMany);
+		
+    	Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
+		InventoryDemand inventoryDemand = readInventoryDemandByCatalogRefIdLocationId(catalogRefId, locationId);
+		
+		int result = validateInventoryRequest(inventory, inventoryDemand, -howMany);
+		
+		System.out.println("RealtimeInventoryServiceImpl decreaseStockLevel result :" + result);
+		
+		return result;
 	}
 	
 	@Override
 	public int increaseStockLevel(String catalogRefId, long howMany, String locationId) {
-		Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
-		return updateStockLevel(inventory, howMany);
+		System.out.println("RealtimeInventoryServiceImpl increaseStockLevel catalogRefId: " + catalogRefId + ", Location: " + locationId + ", howMany: " + howMany);
+		
+    	Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
+		InventoryDemand inventoryDemand = readInventoryDemandByCatalogRefIdLocationId(catalogRefId, locationId);
+		
+		int result = validateInventoryRequest(inventory, inventoryDemand, howMany);
+		
+		System.out.println("RealtimeInventoryServiceImpl increaseStockLevel result :" + result);
+		
+		return result;
 	}
 	
 	@Override
 	public Date queryAvailabilityDate(String catalogRefId, String locationId) {
-		Date result = null;
+		System.out.println("Gigaspace connector queryAvailabilityDate: " + catalogRefId + ", Location: " + locationId );
+		
+    	Date result = null;
 		Inventory inventory = readByCatalogRefIdLocationId(catalogRefId, locationId);
 		if (inventory.getAvailabilityStatus() != AVAILABILITY_STATUS_IN_STOCK) {
 			result = inventory.getAvailabilityDate();
 		}
+		
+		
 		return result;
 	}
 	
@@ -95,7 +143,48 @@ public class RealtimeInventoryServiceImpl implements RealtimeInventoryService {
 		// TODO: Think of quering by ID and using concatination of catalogRefId & locationId as ID for Inventory object.
 		return inventory;
 	}
-	
+	private InventoryDemand readInventoryDemandByCatalogRefIdLocationId(String catalogRefId, String locationId) {
+		// TODO: Implement proper logging
+    	System.out.println("RealtimeInventory readInventoryDemandByCatalogRefIdLocationId: " + catalogRefId + ", Location: " + locationId );
+		
+		SQLQuery<InventoryDemand> query;
+		if (locationId != null) {
+			query = new SQLQuery<InventoryDemand>(InventoryDemand.class, "catalogRefId = ? and locationId = ?")
+					.setParameter(1, catalogRefId)
+					.setParameter(2, locationId);
+		} else {
+			query = new SQLQuery<InventoryDemand>(InventoryDemand.class, "catalogRefId = ? and locationId is null")
+					.setParameter(1, catalogRefId);
+		}
+		
+		InventoryDemand inventory = gigaSpace.read(query);
+		
+		return inventory;
+	}
+	 	private int validateInventoryRequest(Inventory inventory, InventoryDemand inventoryDemand, Long howMany) {
+			// TODO: Implement proper logging
+	    	System.out.println("RealtimeInventory validateInventoryRequest updateStockLevel howMany: " + howMany);
+	    	
+			if (inventory == null) {
+				return INVENTORY_STATUS_ITEM_NOT_FOUND;
+			}
+			
+			long inventoryTotal = (inventory != null)?inventory.getStockLevel():0L;
+			long inventoryDemandTotal = (inventoryDemand != null)?inventoryDemand.getStockLevel():0L;
+			
+			long stockLevel = inventoryTotal - inventoryDemandTotal;
+			
+			System.out.println("validateInventoryRequest updateStockLevel stocklevel :" + stockLevel);
+			
+			if (howMany >= 0 || (howMany < 0 && stockLevel >= Math.abs(howMany))) {
+				//inventory.setStockLevel(stockLevel + howMany);
+				//gigaSpaceRemoteProxy.write(inventory);
+				return INVENTORY_STATUS_SUCCEED;
+			} else {
+				return INVENTORY_STATUS_INSUFFICIENT_SUPPLY;
+			}
+		}
+	/*
 	private int updateStockLevel(Inventory inventory, Long howMany) {
 		// TODO: Implement proper logging
 		System.out.println("In Remote InventoryService - ID: " + inventory + ", HowMany: " + howMany);
@@ -110,9 +199,15 @@ public class RealtimeInventoryServiceImpl implements RealtimeInventoryService {
 		} else {
 			return INVENTORY_STATUS_INSUFFICIENT_SUPPLY;
 		}
-	}
+	}*/
 	
 	public void initInventory(String catalogRefId, String locationId) {
-		gigaSpace.write(new Inventory(1, 100L, catalogRefId, null, 50L, 1000, null));
+		System.out.println("RealtimeInventory catalogRefId: " + catalogRefId + ", locationId: " + locationId );
+		
+		gigaSpace.write(new Inventory(1, 100L, catalogRefId +"1", "NY", 51L, 1000, new Date()));
+		gigaSpace.write(new Inventory(2, 100L, catalogRefId +"2", "NY", 52L, 1000, new Date()));
+		gigaSpace.write(new Inventory(3, 100L, catalogRefId +"3", "CA", 53L, 1000, new Date()));
+		gigaSpace.write(new Inventory(4, 100L, catalogRefId +"4", "CA", 54L, 1000, new Date()));
+		gigaSpace.write(new Inventory(5, 100L, catalogRefId+ "5", "FL", 55L, 1000, new Date()));
 	}
 }
